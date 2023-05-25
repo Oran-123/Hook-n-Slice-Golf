374065a522fdc50e65b515d8f88754001ccf4bf6
Oran-123 <>
2023-05-25 07:30:37 +0000

    register booking models

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import TeeTime, Booking
+from .models import Booking, TeeTime
 
 # Register your models here.
 
-admin.site.register(TeeTime, Booking)
+admin.site.register(Booking)
+admin.site.register(TeeTime)
+
