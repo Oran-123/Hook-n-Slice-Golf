b729e15b11f387b5403f41783e846485178d6a15
Oran-123 <>
2023-06-19 23:33:30 +0000

    add search_field for the username in the booking class

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from rangefilter.filters import DateRangeFilterBuilder, DateTimeRangeFilterBuilder, NumericRangeFilterBuilder
 from .models import Booking, TeeTime
+from django.contrib.auth.models import User
 
 
 @admin.register(Booking)
@@ -8,12 +9,13 @@ class BookingAdmin(admin.ModelAdmin):
     list_display = ('user_name', 'players', 'booking_datetime', 'buggy')
     list_filter = (
         ("booking_datetime", DateRangeFilterBuilder()),)
+    search_fields = ['user_name__username']
 
 
 @admin.register(TeeTime)
 class TeeTimeAdmin(admin.ModelAdmin):
     list_display = ('tee_datetime', 'available', 'booked_players')
-    list_filter = ('available',("tee_datetime", DateRangeFilterBuilder()))
+    list_filter = ('available', ("tee_datetime", DateRangeFilterBuilder()))
 
     def booked_players(self, obj):
         return obj.available_slots()
