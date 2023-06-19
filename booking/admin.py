5b2f2e3a55873918b3d86df8a94aa6f8e4a91f71
Oran-123 <>
2023-06-19 23:13:54 +0000

    create classes to filter booking models on the admin site

--- booking/admin.py
+++ booking/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from rangefilter.filters import DateRangeFilterBuilder, DateTimeRangeFilterBuilder, NumericRangeFilterBuilder
 from .models import Booking, TeeTime
 
-# Register your models here.
 
-admin.site.register(Booking)
-admin.site.register(TeeTime)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'players', 'booking_datetime', 'buggy')
+    list_filter = (
+        ("booking_datetime", DateRangeFilterBuilder()),)
 
+
+@admin.register(TeeTime)
+class TeeTimeAdmin(admin.ModelAdmin):
+    list_display = ('tee_datetime', 'available', 'booked_players')
+    list_filter = ('available',)
+    search_fields = ("tee_datetime", DateRangeFilterBuilder())
+
+    def booked_players(self, obj):
+        return obj.available_slots()
+
+    booked_players.short_description = "Available Spaces"
