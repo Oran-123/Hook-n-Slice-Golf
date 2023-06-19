aa9eefd2c6d0f24bde0c66f2cb8537a01f7f298c
Oran-123 <>
2023-06-19 15:59:31 +0000

    add url for booking app

--- django_hook_n_slice_golf/urls.py
+++ django_hook_n_slice_golf/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from booking.views import booking
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('booking/', booking, name="booking"),
+
 ]
