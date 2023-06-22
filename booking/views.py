781e049d4d75d997e03c0d41acb617e1e2ddd9ce
Oran-123 <>
2023-06-22 19:05:18 +0000

    add views to parse data to booking form and create and save booking object

--- booking/views.py
+++ booking/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Booking, TeeTime
 from django.contrib.auth.decorators import login_required
 from .forms import TeeTimeForm
+from datetime import datetime
+from dateutil import parser
 
 
 @login_required
@@ -24,3 +26,39 @@ def tee_time_booking(request):
     }
 
     return render(request, 'booking.html', context)
+
+
+@login_required
+def booking_form(request, teetime_id):
+    teetime = get_object_or_404(TeeTime, pk=teetime_id)
+    form = TeeTimeForm(request.POST)
+
+    return render(request, 'booking_form.html', {'teetime': teetime, 'form': form})
+
+
+@login_required
+def booking_submit(request):
+
+    if request.method == 'POST':
+        datetime_str = request.POST.get('date & time')
+        players = request.POST.get('players')
+        buggy = request.POST.get('buggy', False)
+        # Convert buggy value to a boolean
+        if buggy == 'on':
+            buggy = True
+
+        booking_datetime = parser.parse(datetime_str)
+        teetime = TeeTime.objects.get(tee_datetime=booking_datetime)
+
+
+        # Process the form data and create the booking
+        Booking(user_name=request.user, players=players,
+                          booking_datetime=teetime, buggy=buggy).save()
+
+        # Redirect the user to the booking success page or any other relevant view
+        return redirect('booking.html')
+        print(Booking)
+
+    return render(request, 'booking.html')
+
+
