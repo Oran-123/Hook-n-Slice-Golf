c8adcf2b5aba6a3086f9f9406d070e0ae9b30123
Oran-123 <>
2023-07-13 22:00:07 +0000

    add modal and delete functionality to user bookings

--- user_profile/views.py
+++ user_profile/views.py
@@ -6,14 +6,17 @@ from booking.models import Booking
 
 @login_required
 def user_profile_bookings(request):
-    filter_type = request.POST.get('filter_type', 'upcoming')  # Default to 'upcoming' if no filter type is selected
+    # Default to 'upcoming' if no filter type is selected
+    filter_type = request.POST.get('filter_type', 'upcoming')
 
     # Fetch the bookings based on the filter type
     if filter_type == 'past':
-        bookings = Booking.objects.filter(user_name=request.user, booking_datetime__tee_datetime__lt=datetime.now()).order_by('-booking_datetime__tee_datetime')
+        bookings = Booking.objects.filter(user_name=request.user, booking_datetime__tee_datetime__lt=datetime.now(
+        )).order_by('-booking_datetime__tee_datetime')
         title = 'Past Bookings'
     else:
-        bookings = Booking.objects.filter(user_name=request.user, booking_datetime__tee_datetime__gte=datetime.now()).order_by('booking_datetime__tee_datetime')
+        bookings = Booking.objects.filter(user_name=request.user, booking_datetime__tee_datetime__gte=datetime.now(
+        )).order_by('booking_datetime__tee_datetime')
         title = 'Upcoming Bookings'
 
     context = {
@@ -22,4 +25,18 @@ def user_profile_bookings(request):
         'title': title,
     }
 
+    if request.method == 'POST':
+        get_booking_id(request)
+
     return render(request, 'user_profile.html', context)
+
+
+def get_booking_id(request):
+    booking_id = request.POST.get('booking_id')
+    if booking_id:
+        delete_booking(booking_id)
+
+
+def delete_booking(booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.delete()
