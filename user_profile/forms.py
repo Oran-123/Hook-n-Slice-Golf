9d00359e1ec1be31cc2a29fa5ddc67b7c981e1cb
Oran-123 <>
2023-07-14 17:36:29 +0000

    Update queryset so only future dates are included and check to ensure selected date is in future

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from booking.models import Booking, TeeTime
+from django.utils import timezone
+
 
 
 class EditBooking(forms.ModelForm):
-    booking_datetime = forms.ModelChoiceField(queryset=TeeTime.objects.all(
-    ), widget=forms.Select(attrs={'class': 'form-control'}))
+    booking_datetime = forms.ModelChoiceField(
+        queryset=TeeTime.objects.filter(tee_datetime__gte=timezone.now()),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
 
     class Meta:
         model = Booking
@@ -13,3 +17,9 @@ class EditBooking(forms.ModelForm):
             'players': forms.NumberInput(attrs={'class': 'form-control', 'min': '1', 'max': '4'}),
             'buggy': forms.CheckboxInput(attrs={'class': 'form-check-input'})
         }
+
+    def clean_booking_datetime(self):
+        booking_datetime = self.cleaned_data.get('booking_datetime')
+        if booking_datetime < timezone.now():
+            raise forms.ValidationError("Cannot select a date in the past.")
+        return booking_datetime
