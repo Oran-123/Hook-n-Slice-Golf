b5e5e769e1d94b2a00dfde0e5870c98085bbd618
Oran-123 <>
2023-07-15 19:45:50 +0000

    fix issues with sign in

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,15 @@ $(document).ready(function () {
     $('#calendar').fullCalendar({
     });
 });
+
+
+function submitLoginFormAndRedirect() {
+    document.querySelector('form.login').submit(); // Submit the form
+
+    // Redirect the user to the desired page
+    if (userIsAdmin) {
+        window.location.href = "{% url 'manage_bookings' %}";  // Redirect to the admin page
+    } else {
+        window.location.href = "{% url 'user_profile' %}";  // Redirect to the user profile page
+    }
+}
