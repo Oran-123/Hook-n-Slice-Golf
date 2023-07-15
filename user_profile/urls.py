25e81c003a347ca2842857abc6d80878c37660be
Oran-123 <>
2023-07-15 19:56:41 +0000

    update the url name for manage bookings

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', user_profile_bookings, name='user_profile_bookings'),
     path('edit_booking/<int:booking_id>/', edit_booking, name='edit_booking'),
     path('manage-bookings/', ManageBookingListView.as_view(),
-         name='manage_bookings'),
+         name='ManageBookingListView'),
 
 ]
