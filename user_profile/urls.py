53359e0cfb0c3d340315039311c6b6bdf341ce5f
Oran-123 <>
2023-07-19 19:57:44 +0000

    update url for deleting booking view

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from user_profile.views import user_profile_bookings, edit_booking, ManageBookingListView, admin_delete_booking
+from user_profile.views import user_profile_bookings, edit_booking, ManageBookingListView, delete_booking
 
 
 urlpatterns = [
 
     path('', user_profile_bookings, name='user_profile_bookings'),
     path('edit_booking/<int:booking_id>/', edit_booking, name='edit_booking'),
+    path('delete_booking/', delete_booking, name='delete_booking'),
     path('manage-bookings/', ManageBookingListView.as_view(),
          name='ManageBookingListView'),
-    path('admin-delete-booking/', ManageBookingListView.as_view(admin_delete_booking),
-         name='admin_delete_booking'),
+
 
 
 
