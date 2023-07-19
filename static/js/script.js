64f1a721d7154a6bae227766d3fe371546c922f0
Oran-123 <>
2023-07-19 19:56:39 +0000

    add function to ensure the booking id is added to the form submitted by super users to delete bookings

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,6 @@ $(document).ready(function () {
     });
 });
 
-
-function submitLoginFormAndRedirect() {
-    document.querySelector('form.login').submit(); // Submit the form
-
-    // Redirect the user to the desired page
-    if (userIsAdmin) {
-        window.location.href = "{% url 'manage_bookings' %}";  // Redirect to the admin page
-    } else {
-        window.location.href = "{% url 'user_profile' %}";  // Redirect to the user profile page
-    }
+function setBookingId(bookingId) {
+    document.getElementById('bookingIdInput').value = bookingId;
 }
