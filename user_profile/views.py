768a651276fc3eae5c75bcb7b294da6fce780b61
Oran-123 <>
2023-07-19 19:54:27 +0000

    combine seperate methodes into a single delete method

--- user_profile/views.py
+++ user_profile/views.py
@@ -5,7 +5,10 @@ from booking.models import Booking
 from django.shortcuts import get_object_or_404
 from .forms import EditBooking
 from django.views.generic import ListView
+from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 @login_required
@@ -29,21 +32,18 @@ def user_profile_bookings(request):
         'title': title,
     }
 
-    if request.method == 'POST':
-        get_booking_id(request)
-
     return render(request, 'user_profile.html', context)
 
 
-def get_booking_id(request):
+def delete_booking(request):
     booking_id = request.POST.get('booking_id')
     if booking_id:
-        delete_booking(booking_id)
-
-
-def delete_booking(booking_id):
-    booking = get_object_or_404(Booking, id=booking_id)
-    booking.delete()
+        booking = get_object_or_404(Booking, id=booking_id)
+        booking.delete()
+        messages.success(request, 'Booking deleted successfully.')
+    else:
+        messages.warning(request, 'Error deleting booking.')
+    return redirect(request.META.get('HTTP_REFERER'))
 
 
 def edit_booking(request, booking_id):
@@ -68,7 +68,3 @@ class ManageBookingListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
 
     def test_func(self):
         return self.request.user.is_superuser
-
-    def admin_delete_booking(self):
-        if request.method == 'POST':
-            get_booking_id(request)
