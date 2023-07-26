8d1aae0ea8fa79cb676c7b0017f135aad82f2be5
Oran-123 <>
2023-07-26 22:15:21 +0000

    import datetime and set min date field to today

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from booking.models import Booking, TeeTime
 from django.utils import timezone
+from datetime import date
 
 
 class EditBooking(forms.ModelForm):
-    
+
     class Meta:
         model = Booking
-        fields = ['booking_date','booking_time', 'players', 'buggy']
+        fields = ['booking_date', 'booking_time', 'players', 'buggy']
         widgets = {
             'players': forms.NumberInput(attrs={'class': 'form-control', 'min': '1', 'max': '4'}),
             'buggy': forms.CheckboxInput(attrs={'class': 'form-check-input'})
-            'booking_date'forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+            'booking_date'forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min': datetime.today().date()})
             'booking_time'forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'})
         }
 
