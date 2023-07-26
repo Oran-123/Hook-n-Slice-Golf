a0eb55d04d19fdb22b6d265ae350e065f3003af7
Oran-123 <>
2023-07-26 22:18:44 +0000

    resolve syntax erorr

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -11,9 +11,9 @@ class EditBooking(forms.ModelForm):
         fields = ['booking_date', 'booking_time', 'players', 'buggy']
         widgets = {
             'players': forms.NumberInput(attrs={'class': 'form-control', 'min': '1', 'max': '4'}),
-            'buggy': forms.CheckboxInput(attrs={'class': 'form-check-input'})
-            'booking_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min': datetime.today().date()})
-            'booking_time': forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'})
+            'buggy': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'booking_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min': datetime.today().date()}),
+            'booking_time': forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'}),
         }
 
 
