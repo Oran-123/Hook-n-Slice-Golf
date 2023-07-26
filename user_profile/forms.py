7de8ddcf0c147f666036573d1d1ae8c2c76580e7
Oran-123 <>
2023-07-26 22:14:17 +0000

    update the fields to reflect the booking date and booking time

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -7,7 +7,7 @@ class EditBooking(forms.ModelForm):
     
     class Meta:
         model = Booking
-        fields = ['booking_datetime', 'players', 'buggy']
+        fields = ['booking_date','booking_time', 'players', 'buggy']
         widgets = {
             'players': forms.NumberInput(attrs={'class': 'form-control', 'min': '1', 'max': '4'}),
             'buggy': forms.CheckboxInput(attrs={'class': 'form-check-input'})
@@ -16,7 +16,7 @@ class EditBooking(forms.ModelForm):
         }
 
     def clean_booking_datetime(self):
-        booking_datetime = self.cleaned_data.get('booking_datetime')
+        booking_datetime = self.cleaned_data.get('booking_date')
         if booking_datetime.tee_datetime < timezone.now():
             raise forms.ValidationError("Cannot select a date in the past.")
         return booking_datetime
