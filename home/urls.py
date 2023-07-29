87b73f70731ad17fe97dae754a4144ee7a53ab3b
Oran-123 <>
2023-07-29 00:17:39 +0000

    add template for 403 error

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls import handler404
+from home.views import custom_404_view
+
+
 
 
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+handler404 = custom_404_view
