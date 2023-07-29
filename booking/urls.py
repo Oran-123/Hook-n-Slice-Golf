26b884667a4c904fe5bcd28556b051d521e5be22
Oran-123 <>
2023-07-29 00:03:19 +0000

    add url for the booking sucess page

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from booking.views import tee_time_booking, booking_form, booking_submit
+from . import views
+
 
 
 urlpatterns = [
@@ -7,4 +9,6 @@ urlpatterns = [
     path('createbooking/', tee_time_booking, name='tee_time_booking'),
     path('<int:teetime_id>/', booking_form, name='booking_form'),
     path('submit/', booking_submit, name='booking_submit'),
+    path('booking/booking_success/<int:booking_id>/', views.booking_success, name='booking_success'),
+
 ]
