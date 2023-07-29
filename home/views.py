e217669072592742788ec0ec9fa947c372dbc393
Oran-123 <>
2023-07-29 00:22:17 +0000

    add method for the error 500

--- home/views.py
+++ home/views.py
@@ -11,3 +11,7 @@ def custom_404_view(request, exception):
 
 def custom_403_view(request, exception):
     return render(request, '403.html', status=403)
+
+
+def custom_500_view(request, exception):
+    return render(request, '500.html', status=500)
