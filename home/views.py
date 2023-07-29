b59c1063d1db42012bf254dec3d1e73c2631ef19
Oran-123 <>
2023-07-29 00:12:03 +0000

    add view for 404 template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def home(request):
     return render(request, 'index.html')
+
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
