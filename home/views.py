4effb5b3896357e3536072851dc6765d315ced80
Oran-123 <>
2023-07-29 00:18:28 +0000

    add method for the 403 error template

--- home/views.py
+++ home/views.py
@@ -7,3 +7,7 @@ def home(request):
 
 def custom_404_view(request, exception):
     return render(request, '404.html', status=404)
+
+
+def custom_403_view(request, exception):
+    return render(request, '403.html', status=403)
