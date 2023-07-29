4bd43e521875a9ad092688a9b54abe26b7a62af8
Oran-123 <>
2023-07-29 20:42:22 +0000

    remove trailing /

--- booking/urls.py
+++ booking/urls.py
@@ -3,12 +3,12 @@ from booking.views import tee_time_booking, booking_form, booking_submit
 from . import views
 
 
-
 urlpatterns = [
 
     path('createbooking', tee_time_booking, name='tee_time_booking'),
-    path('<int:teetime_id>/', booking_form, name='booking_form'),
+    path('<int:teetime_id>', booking_form, name='booking_form'),
     path('submit/', booking_submit, name='booking_submit'),
-    path('booking/booking_success/<int:booking_id>/', views.booking_success, name='booking_success'),
+    path('booking/booking_success/<int:booking_id>',
+         views.booking_success, name='booking_success'),
 
 ]
