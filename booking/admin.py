1ef7a28037c917b0432c1b3e7d4bcdbb247a8923
Oran-123 <>
2023-07-30 10:55:02 +0000

    Add doc strings to each method

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,12 @@
+"""
+Booking App - Views
+---------------------
+Views for booking app
+
+"""
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from django.contrib import admin
 from rangefilter.filters import (
     DateRangeFilterBuilder,
@@ -6,10 +15,14 @@ from rangefilter.filters import (
 )
 from .models import Booking, TeeTime
 from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Booking model.
+    """
     list_display = ('user_name', 'players', 'booking_datetime', 'buggy')
     list_filter = (
         ("booking_datetime", DateRangeFilterBuilder()),)
@@ -18,6 +31,9 @@ class BookingAdmin(admin.ModelAdmin):
 
 @admin.register(TeeTime)
 class TeeTimeAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for TeeTime model.
+    """
     list_display = ('tee_datetime', 'available', 'booked_players')
     list_filter = ('available', ("tee_datetime", DateRangeFilterBuilder()))
 
